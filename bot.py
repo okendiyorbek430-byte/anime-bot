fc52a1fe1acfb3a9645a0c1c9e38d16af7b5a1c0
okendiyorbek430-byte <okendiyorbek430@gmail.com>
2026-02-28 13:31:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("8573220797:AAGBgRKid5tK3JksBmnEhNjyDez4a_UCkh4")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Anime bot ishlayapti 🔥")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Bot started...")
+app.run_polling()
